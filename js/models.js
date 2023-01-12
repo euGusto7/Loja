985431d16eec353ceced6a161783637e80a261b1
Guilherme Augusto <102872656+euGusto7@users.noreply.github.com>
2023-01-12 14:03:35 -0300

    Add files via upload

---
+++ js/models.js
@@ -0,0 +1,22 @@
+/*
+=========================================================================
+Nome do Projeto: Carrinho de Compras em Javascript
+Finalidade: Cursos de Informática do IFSP campus Caraguatatuba 
+Fonte Base: Projetos do Curso B7Web - https://b7web.com.br/ 
+Autor: Denny Paulista Azevedo Filho
+License: MIT License
+========================================================================= 
+*/
+
+let modelsJson = [
+    {id:1, name:'Lola Gliter Roxa', img:'img/s1.png', price:[30.00, 30.00, 30.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:2, name:'Lola Gliter Branco', img:'img/s2.png', price:[30.00, 30.00, 30.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:3, name:'Havainas Gliter Pink', img:'img/s3.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:4, name:'Havainas Glter Branco', img:'img/s4.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:5, name:'Havaianas Gliter Preto', img:'img/s5.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:6, name:'Havainas Temático Girassol', img:'img/s6.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:7, name:'Havainas Temático Cristais', img:'img/s7.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:8, name:'Rasteirinha Branca', img:'img/s8.png', price:[30.00, 30.00, 30.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:9, name:'Rasteirinha Pink', img:'img/s9.png', price:[30.00, 30.00, 30.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+    {id:10, name:'Rasteirinha Dourada', img:'img/s10.png', price:[35.00, 35.00, 35.00], sizes:['2/50', '2/48', '2/32' , '2/50'], description:'Estoque Disponível'},
+];
